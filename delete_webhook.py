a2a96f06a48f6a91a828b6891e866809e1addb0d
Yuuuiiiiiiiiiiiii <juncai050215@gmail.com>
2025-10-27 12:50:52 +0800

    fix order record

--- delete_webhook.py
+++ delete_webhook.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 CLICKUP_TOKEN = os.getenv("CLICKUP_TOKEN")
-WEBHOOK_ID = "b12458fa-3c70-42ec-a702-110ea7ac0247"
+WEBHOOK_ID = "8751b310-6861-4726-a2b2-49f96fb1f509"
 
 url = f"https://api.clickup.com/api/v2/webhook/{WEBHOOK_ID}"
 headers = {
